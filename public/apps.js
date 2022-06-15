b8d7eb50a740629c6caef4c6d6fad499a91acec6
towhid <niamaaktar@gmail.com>
2022-06-16 03:19:22 +0600

    Public folder gets updated

--- public/apps.js
+++ public/apps.js
@@ -1,5 +1,23 @@
 // import { init } from "ityped";
-
-console.log("hello");
 const introSkill = document.querySelector(".intro_skill");
-console.log(introSkill);
+const header = document.querySelector(".header");
+const navContainer = document.querySelector(".nav_container");
+const aboutContainer = document.querySelector(".about_section");
+
+const navContainerHeight = navContainer.getBoundingClientRect().height;
+
+const callBack = (entries, observer) => {
+  const [entry] = entries;
+
+ /*  if (!entry.isIntersecting) navContainer.classList.add("sticky");
+  else navContainer.classList.remove("sticky"); */
+};
+
+const observerOptions = {
+  root: null,
+  threshold: 0,
+  rooMarging: `-${navContainerHeight}px`,
+};
+
+const observer = new IntersectionObserver(callBack, observerOptions);
+observer.observe(header);
