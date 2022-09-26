d899a07aa94e79abbfd7114b440e495cb41554b7
towhid <niamaaktar@gmail.com>
2022-09-26 13:23:39 +0600

    all Changes has been updated

--- frontend/src/pages/introSection.js
+++ frontend/src/pages/introSection.js
@@ -18,68 +18,73 @@ import { BsSunFill } from "react-icons/bs";
 import Towhid from "../assets/towhid-small.jpg";
 import "../style/container.css";
 
+import heroImage1 from "../assets/heroimage1.jpg";
 const IntroSection = () => {
   return (
     <>
       <header
-        class="header"
+        className="header"
         style={{
           border: "2px solid blue",
+          // backgroundImage: `url(${heroImage1})`,
+          // backgroundRepeat: "noreapet",
+          // backgroundSize: "cover",
+          // height: "100%",
         }}
       >
         <div className="container">
-          <div class="intro_container area_definer1">
-            <div class="intro_child intro_child-1">
-              <h1 class="intro_title">
+          <div className="intro_container">
+            <div className="intro_child intro_child-1">
+              <h1 className="intro_title">
                 My name is <br />
                 <strong>Towhidul Islam,</strong>
                 I'm a <br />
               </h1>
-              <span class="intro_skill">Frontend Developer</span>
-              <p class="intro_des">
+              <span className="intro_skill">Frontend Developer</span>
+              <p className="intro_des">
                 To Know about me more just go through my about section. I have
                 explained everyting up there elaborately
               </p>
-              <div class="intro_links">
-                <button class="intro_btn ripple_btn intro_btn-1">
+              <div className="intro_links">
+                <button className="intro_btn ripple_btn intro_btn-1">
                   let's Talk
                 </button>
-                <button class="intro_btn ripple_btn intro_btn-1">
+                <button className="intro_btn ripple_btn intro_btn-1">
                   Portfolio
                 </button>
               </div>
-              <div class="intro_icons_container">
-                <div class="intro_social_links">
-                  <p class="intro_social_link_title">Follow me on</p>
+              <div className="intro_icons_container">
+                <div className="intro_social_links">
+                  <p className="intro_social_link_title">Follow me on</p>
                   <a
                     href="https://www.facebook.com/T.TECHINTIMACY"
-                    class="intro_social_link_links"
+                    className="intro_social_link_links"
                   >
                     <FaFacebook />
                   </a>
                   <a
                     href="www.linkedin.com/in/imtowhidulislam"
-                    class="intro_social_link_links"
+                    className="intro_social_link_links"
                   >
                     <FaLinkedin />
                   </a>
                   <a
                     href="https://github.com/imtowhidulislam"
-                    class="intro_social_link_links"
+                    className="intro_social_link_links"
                   >
                     <FaGithubSquare />
                   </a>
                   <a
                     href="https://www.instagram.com/thisistowhid/"
-                    class="intro_social_link_links"
+                    className="intro_social_link_links"
                   >
                     <FaInstagram />
                   </a>
                 </div>
               </div>
             </div>
-            <div class="intro_child intro_child-2">
-              <img src={Towhid} alt="intro image" class="intro_img" />
+            <div className="intro_child intro_child-2">
+              <img src={Towhid} alt="intro image" className="intro_img" />
             </div>
           </div>
         </div>
